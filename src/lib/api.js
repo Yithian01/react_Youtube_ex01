38081449788de84b920b2c385c7113f720613cd9
Yithian01 <Yithian01>
2023-02-08 15:01:38 +0900

    env

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,7 @@
-const API_KEY=`AIzaSyAc4Eg7v57OwhEigDTQiIyyP46FzbbrKks`;
+
+const API_KEY=process.env.REACT_APP_API_KEY;
+
+//const API_KEY=`AIzaSyAc4Eg7v57OwhEigDTQiIyyP46FzbbrKks`;
 
 export const videoUrl=`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=30&regionCode=kr&key=${API_KEY}`
 
